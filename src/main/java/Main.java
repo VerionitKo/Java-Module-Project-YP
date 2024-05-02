ff8ac8db47feaf89fe89ef2af547ab465f04ff4f
Kurilenko Aleksandr <me@nobito.ru>
2024-05-02 23:03:20 +0300

    first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner sc = new Scanner(System.in);
+        String goodName;
+        int peopleCount;
+
+        System.out.println("Привет!\nНа скольких человек будем делить счёт?" +
+                " \n\uD83D\uDC49 введи значение: ");
+
     }
 }
